a1877de28639cf6799740017d523c7649bedbff3
Paramuth Samuthrsindh <paramuth@csail.mit.edu>
2024-11-12 15:16:23 -0500

    Add result

--- parallel/spmv/run_spmv.jl
+++ parallel/spmv/run_spmv.jl
@@ -36,9 +36,9 @@ parsed_args = parse_args(ARGS, s)
 # Mapping from dataset types to datasets
 datasets = Dict(
     "uniform" => [
-        OrderedDict("size" => 1_000, "sparsity" => 0.1),
-        OrderedDict("size" => 10_000, "sparsity" => 0.1),
-        OrderedDict("size" => 1_000_000, "sparsity" => 3_000_000)
+        OrderedDict("size" => 2^10, "sparsity" => 0.1),
+        OrderedDict("size" => 2^13, "sparsity" => 0.1),
+        OrderedDict("size" => 2^20, "sparsity" => 3_000_000)
     ],
     "FEMLAB" => [
         "FEMLAB/poisson3Da",
