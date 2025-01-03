3b8186a73b1d90c9f5e0cb7f7eeaed419b627d23
Willow Ahrens <willow.marie.ahrens@gmail.com>
2025-01-03 11:36:08 -0500

    fix

--- images/download_masks.jl
+++ images/download_masks.jl
@@ -1,8 +1,10 @@
 #!/usr/bin/env julia
 if abspath(PROGRAM_FILE) == @__FILE__
     using Pkg
-    Pkg.activate(@__DIR__)
+    Pkg.activate(joinpath(@__DIR__, ".."))
     Pkg.instantiate()
+    Pkg.status("Finch")
+    println("Julia Version: $(VERSION)")
 end
 
 using Finch
