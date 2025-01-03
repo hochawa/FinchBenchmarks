d25bcab7dd85ddb2ce3a7410c930ea4b87cdce89
Willow Ahrens <willow@lanka25.csail.mit.edu>
2025-01-03 11:22:49 -0500

    fix

--- catjson.jl
+++ catjson.jl
@@ -1,3 +1,12 @@
+#!/usr/bin/env julia
+if abspath(PROGRAM_FILE) == @__FILE__
+    using Pkg
+    Pkg.activate(@__DIR__)
+    Pkg.instantiate()
+end
+include(joinpath(@__DIR__, "deps/diagnostics.jl"))
+print_diagnostics()
+
 using JSON
 
 res = []
