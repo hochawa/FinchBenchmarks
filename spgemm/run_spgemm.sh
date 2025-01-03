78b0966f2c157e377542fd8caaa1819e7e74299e
Willow Ahrens <willow.marie.ahrens@gmail.com>
2025-01-03 14:06:15 -0500

    fix

--- spgemm/run_spgemm.sh
+++ spgemm/run_spgemm.sh
@@ -2,6 +2,9 @@
 
 julia run_spgemm.jl -d "zhang_small" --kernels "all" -o results_zhang_small.json
 #julia run_spgemm.jl -d "zhang_large" --kernels "fast" -o results_zhang_large.json
+#Ideally we would have time to run the large matrices. Alternatively we can copy
+#the reference results so that the plotting script doesn't error out
+cp reference_results_zhang_large.json results_zhang_large.json 
 julia run_spgemm.jl -d "scale" --kernels "all" -b 1 -B 9 -o split_results_scale_1.json
 julia run_spgemm.jl -d "scale" --kernels "all" -b 2 -B 9 -o split_results_scale_2.json
 julia run_spgemm.jl -d "scale" --kernels "all" -b 3 -B 9 -o split_results_scale_3.json
