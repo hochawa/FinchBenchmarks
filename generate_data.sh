a23e8d6c13e2cabfb613b170c20c9781f65df183
Willow Ahrens <willow@slurm-login-0.csail.mit.edu>
2025-01-03 21:46:03 -0500

    add modifications

--- generate_data.sh
+++ generate_data.sh
@@ -1,4 +1,7 @@
+cd graphs; wget https://nrvis.com/download/data/soc/soc-orkut.zip -O soc-orkut.zip; unzip soc-orkut.zip; cd -
+
+cd spgemm; julia make_scale.jl; cd -
+
 cd graphs; ./rmat_gen 22 64; cd -
 cd graphs; ./rmat_gen 23 32; cd -
 cd graphs; ./rmat_gen 24 16; cd -
-cd spgemm; julia make_scale.jl; cd -
